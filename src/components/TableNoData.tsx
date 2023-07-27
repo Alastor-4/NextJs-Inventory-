305891a722fe9cfa8b35b0743c11a3dbb92ae115
Dayan Gomez Abad <dgabad91@gmail.com>
2023-07-27 16:22:18 -0400

    table no data component

---
+++ src/components/TableNoData.tsx
@@ -0,0 +1,22 @@
+import {Box} from "@mui/material";
+import {InfoOutlined} from "@mui/icons-material";
+
+export const TableNoData = () => {
+    return (
+        <Box
+            sx={
+                {
+                    width: "100%",
+                    height: "100px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#a9afbb"
+                }
+            }
+        >
+            <InfoOutlined sx={{mr: "5px"}}/>
+            No hay datos que mostrar
+        </Box>
+    )
+}
