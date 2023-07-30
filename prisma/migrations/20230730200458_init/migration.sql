2a22ac6b0ec0fe017d2c626e845a2f9bccefe7f3
Dayan Gomez Abad <dgabad91@gmail.com>
2023-07-30 16:06:44 -0400

    improve schema. running initial migration

--- prisma/migrations/20230730200458_init/migration.sql
+++ prisma/migrations/20230730200458_init/migration.sql
@@ -13,12 +13,24 @@ CREATE TABLE `charateristics` (
 -- CreateTable
 CREATE TABLE `departments` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `store_id` INTEGER NULL,
     `name` VARCHAR(255) NULL,
     `description` VARCHAR(255) NULL,
     `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
-    INDEX `store_id`(`store_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `depots` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `product_id` INTEGER NULL,
+    `warehouse_id` INTEGER NULL,
+    `inserted_by_id` INTEGER NULL,
+    `product_total_units` INTEGER NULL,
+    `product_total_remaining_units` INTEGER NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `warehouse_id`(`warehouse_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -26,11 +38,10 @@ CREATE TABLE `departments` (
 CREATE TABLE `products` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `department_id` INTEGER NULL,
+    `owner_id` INTEGER NULL,
     `name` VARCHAR(255) NULL,
     `description` VARCHAR(255) NULL,
-    `units_quantity` INTEGER NULL,
     `buy_price` DOUBLE NULL,
-    `seller_profit_percentage` FLOAT NULL,
     `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
     INDEX `department_id`(`department_id`),
@@ -40,7 +51,7 @@ CREATE TABLE `products` (
 -- CreateTable
 CREATE TABLE `products_sell` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `product_id` INTEGER NULL,
+    `store_depot_id` INTEGER NULL,
     `units_quantity` INTEGER NULL,
     `unit_buy_price` DOUBLE NULL,
     `total_price` DOUBLE NULL,
@@ -49,7 +60,7 @@ CREATE TABLE `products_sell` (
     `units_returned_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
-    INDEX `product_id`(`product_id`),
+    INDEX `store_depot_id`(`store_depot_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -67,18 +78,33 @@ CREATE TABLE `roles` (
 -- CreateTable
 CREATE TABLE `sell_price_history` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `product_id` INTEGER NULL,
-    `buy_price` DOUBLE NULL,
+    `store_depot_id` INTEGER NULL,
+    `sell_price` DOUBLE NULL,
     `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
-    INDEX `product_id`(`product_id`),
+    INDEX `store_depot_id`(`store_depot_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `store_depots` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `store_id` INTEGER NULL,
+    `depot_id` INTEGER NULL,
+    `product_units` INTEGER NULL,
+    `product_remaining_units` INTEGER NULL,
+    `seller_profit_percentage` FLOAT NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `depot_id`(`depot_id`),
+    INDEX `store_id`(`store_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `stores` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `owner_user_id` INTEGER NULL,
+    `owner_id` INTEGER NULL,
     `name` VARCHAR(255) NULL,
     `description` VARCHAR(255) NULL,
     `slogan` VARCHAR(255) NULL,
@@ -87,8 +113,7 @@ CREATE TABLE `stores` (
     `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
     UNIQUE INDEX `name`(`name`),
-    INDEX `owner_user_id`(`owner_user_id`),
-    INDEX `seller_user_id`(`seller_user_id`),
+    INDEX `owner_id`(`owner_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -109,26 +134,48 @@ CREATE TABLE `users` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `warehouses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `owner_id` INTEGER NULL,
+    `name` VARCHAR(255) NULL,
+    `description` VARCHAR(255) NULL,
+    `address` VARCHAR(255) NULL,
+    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `owner_id`(`owner_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `charateristics` ADD CONSTRAINT `charateristics_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- AddForeignKey
-ALTER TABLE `departments` ADD CONSTRAINT `departments_ibfk_1` FOREIGN KEY (`store_id`) REFERENCES `stores`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `depots` ADD CONSTRAINT `depots_ibfk_1` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- AddForeignKey
+ALTER TABLE `depots` ADD CONSTRAINT `depots_ibfk_2` FOREIGN KEY (`warehouse_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- AddForeignKey
 ALTER TABLE `products` ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- AddForeignKey
-ALTER TABLE `products_sell` ADD CONSTRAINT `products_sell_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `products_sell` ADD CONSTRAINT `products_sell_ibfk_1` FOREIGN KEY (`store_depot_id`) REFERENCES `store_depots`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- AddForeignKey
-ALTER TABLE `sell_price_history` ADD CONSTRAINT `sell_price_history_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `sell_price_history` ADD CONSTRAINT `sell_price_history_ibfk_1` FOREIGN KEY (`store_depot_id`) REFERENCES `store_depots`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- AddForeignKey
-ALTER TABLE `stores` ADD CONSTRAINT `stores_ibfk_1` FOREIGN KEY (`owner_user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `store_depots` ADD CONSTRAINT `store_depots_ibfk_1` FOREIGN KEY (`store_id`) REFERENCES `stores`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- AddForeignKey
-ALTER TABLE `stores` ADD CONSTRAINT `stores_ibfk_2` FOREIGN KEY (`seller_user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `store_depots` ADD CONSTRAINT `store_depots_ibfk_2` FOREIGN KEY (`depot_id`) REFERENCES `depots`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- AddForeignKey
+ALTER TABLE `stores` ADD CONSTRAINT `stores_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 -- AddForeignKey
 ALTER TABLE `users` ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- AddForeignKey
+ALTER TABLE `warehouses` ADD CONSTRAINT `warehouses_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
