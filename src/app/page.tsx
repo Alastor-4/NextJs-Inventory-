3d8b0b3047c63f0e03c0e52c86737042afa44f05
Dayan Gomez Abad <dgabad91@gmail.com>
2023-08-06 15:06:52 -0400

    remove tailwind library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,56 @@
-import Image from 'next/image'
 import Link from "next/link"
 import Login from "@/components/Login";
+import {Grid} from "@mui/material";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Login/>
+    return (
+        <main>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Login/>
+                </Grid>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link href={"/role"} className="font-semibold group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          Roles{' '}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-        </Link>
+                <Grid container item xs={12}>
+                    <Grid item xs={3}>
+                        <Link href={"/role"}>
+                            Roles{' '}-->>
+                        </Link>
+                    </Grid>
 
-        <Link href={"/user"} className="font-semibold group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          Users{' '}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-        </Link>
+                    <Grid item xs={3}>
+                        <Link href={"/user"}>
+                            Users{' '}-->>
+                        </Link>
+                    </Grid>
 
-        <Link href={"/warehouse"} className="font-semibold group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          Warehouses{' '}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-        </Link>
+                    <Grid item xs={3}>
+                        <Link href={"/warehouse"}>
+                            Warehouses{' '}-->>
+                        </Link>
+                    </Grid>
 
-        <Link href={"/store"} className="font-semibold group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          Stores{' '}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-        </Link>
+                    <Grid item xs={3}>
+                        <Link href={"/store"}>
+                            Stores{' '}-->>
+                        </Link>
+                    </Grid>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                    <Grid item xs={3}>
+                        <a
+                            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <h2>
+                                Deploy{' '}-->>
+                            </h2>
+                            <p>
+                                Instantly deploy your Next.js site.
+                            </p>
+                        </a>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </main>
+    )
 }
