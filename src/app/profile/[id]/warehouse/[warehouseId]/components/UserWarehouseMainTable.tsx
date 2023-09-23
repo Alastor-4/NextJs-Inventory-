e41dff0f3d94c74d44b3780d59a545f65132628d
dgabad <dgabad91@gmail.com>
2023-09-23 19:31:37 -0400

    load depot store distribution

--- src/app/profile/[id]/warehouse/[warehouseId]/components/UserWarehouseMainTable.tsx
+++ src/app/profile/[id]/warehouse/[warehouseId]/components/UserWarehouseMainTable.tsx
@@ -24,7 +24,7 @@ import {TableNoData} from "@/components/TableNoData";
 import {
     Add,
     AddOutlined,
-    ArrowLeft,
+    ArrowLeft, ChevronRightOutlined,
     DeleteOutline,
     Done,
     EditOutlined, ExpandLessOutlined, ExpandMoreOutlined, ShareOutlined, Visibility, VisibilityOutlined,
@@ -441,6 +441,13 @@ export default function UserWarehouseMainTable(props) {
         setOpenImagesDialog(true)
     }
 
+    const [storesDepotDistribution, setStoresDepotDistribution] = React.useState(null)
+    async function handleLoadStoresDistribution(depot) {
+        const response = await warehouseDepots.depotStoreDistribution(ownerId, warehouseDetails.id, depot.id)
+        if (response)
+            setStoresDepotDistribution(response)
+    }
+
     //expand description
     const [expandIndex, setExpandIndex] = React.useState(null)
 
@@ -625,7 +632,7 @@ export default function UserWarehouseMainTable(props) {
                                                         row.images.length > 0
                                                             ? (
                                                                 <Box
-                                                                    sx={{cursor: "pointer", display: "flex", alignItems: "center"}}
+                                                                    sx={{cursor: "pointer", display: "flex", alignItems: "center", color: "blue"}}
                                                                     onClick={() => handleOpenImagesDialog(row.images)}
                                                                 >
                                                                     {row.images.length}
@@ -650,6 +657,38 @@ export default function UserWarehouseMainTable(props) {
                                                     {row.depots[0].product_total_units ?? "-"} de {row.depots[0].product_total_remaining_units ?? "-"}
                                                 </Grid>
                                             </Grid>
+
+                                            <Grid container item spacing={1} xs={12}>
+                                                <Grid item xs={"auto"} sx={{color: "blue"}}>
+                                                    <Box
+                                                        sx={{cursor: "pointer", display: "flex", alignItems: "center"}}
+                                                        onClick={() => handleLoadStoresDistribution(row.depots[0])}
+                                                    >
+                                                        Ver distribución del producto
+                                                        <ShareOutlined fontSize={"small"} sx={{ml: "5px"}}/>
+                                                    </Box>
+                                                </Grid>
+                                            </Grid>
+
+                                            {
+                                                storesDepotDistribution && (
+                                                    storesDepotDistribution.map(item => (
+                                                        <Grid container item spacing={1} xs={12} key={item.id}>
+                                                            <Grid item xs={"auto"} sx={{fontWeight: 600, display: "flex", alignItems: "center"}}>
+                                                                <ChevronRightOutlined fontSize={"small"}/>
+                                                                {item.name}:
+                                                            </Grid>
+                                                            <Grid item xs={true}>
+                                                                {
+                                                                    item.store_depots.length > 0
+                                                                        ? `Total (${item.store_depots[0].product_units}) - Restantes (${item.store_depots[0].product_remaining_units})`
+                                                                        : "no asignado"
+                                                                }
+                                                            </Grid>
+                                                        </Grid>
+                                                    ))
+                                                )
+                                            }
                                         </Grid>
                                     </Collapse>
                                 </TableCell>
--- src/app/profile/[id]/warehouse/[warehouseId]/requests/warehouseDepots.ts
+++ src/app/profile/[id]/warehouse/[warehouseId]/requests/warehouseDepots.ts
@@ -2,6 +2,7 @@ import apiRequest from "@/api"
 
 const url = (ownerId: string, warehouseId: string) => `/profile/${ownerId}/warehouse/${warehouseId}/api`
 const urlCreate = (ownerId: string, warehouseId: string) => `/profile/${ownerId}/warehouse/${warehouseId}/create/api`
+const urlDepotAssign = (ownerId: string, warehouseId: string) => `/profile/${ownerId}/warehouse/${warehouseId}/depot-assign/api`
 
 const warehouseDepots = {
     allDepots: async function (userId: string, warehouseId: string) {
@@ -18,6 +19,17 @@ const warehouseDepots = {
     allProductsWithoutDepots: async function (userId: string, warehouseId: string) {
         try {
             const response = await apiRequest.get(urlCreate(userId, warehouseId))
+            return response.data
+        } catch (e) {
+            //ToDo: notify error here
+        }
+
+        return false
+    },
+
+    depotStoreDistribution: async function (userId: string, warehouseId: string, depotId: string) {
+        try {
+            const response = await apiRequest.get(urlDepotAssign(userId, warehouseId), {params: {depotId: depotId}})
             return response.data
         } catch (e) {
             //ToDo: notify error here
