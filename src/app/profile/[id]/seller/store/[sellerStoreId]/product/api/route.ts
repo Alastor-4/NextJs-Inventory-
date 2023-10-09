ba9dd68c6e6068a4d1d9fff8fffceeecd5a730fa
dgabad <dgabad91@gmail.com>
2023-10-08 21:16:24 -0400

    store actions component

---
+++ src/app/profile/[id]/seller/store/[sellerStoreId]/product/api/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from 'next/server'
+import {prisma} from "db";
+
+// Get store products
+export async function GET(request: Request, { params }: { params: { sellerStoreId: string } }) {
+    const storeId = parseInt(params.sellerStoreId)
+
+    const storeProducts = await prisma.departments.findMany({
+        where: {
+            products: {
+                some: {
+                    depots: {
+                        some: {
+                            store_depots: {
+                                some: {
+                                    store_id: storeId
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        include: {
+            products: {
+                where: {
+                    depots: {
+                        some: {
+                            store_depots: {
+                                some: {
+                                    store_id: storeId
+                                }
+                            }
+                        }
+                    }
+                },
+                include: {
+                    departments: true,
+                    depots: {
+                        include: {
+                            store_depots: true
+                        }
+                    },
+                    characteristics: true,
+                    images: true,
+                },
+            },
+        },
+    })
+
+    return NextResponse.json(storeProducts)
+}
+
