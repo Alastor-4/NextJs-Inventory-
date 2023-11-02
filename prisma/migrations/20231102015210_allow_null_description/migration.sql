cc603e85dd977654a713357c313b577bf7829819
dgabad <dgabad91@gmail.com>
2023-11-01 21:56:45 -0400

    status allow null

---
+++ prisma/migrations/20231102015210_allow_null_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `products_reservation` MODIFY `status_description` TEXT NULL;
--- src/app/profile/[id]/seller/store/[sellerStoreId]/reservation/api/route.ts
+++ src/app/profile/[id]/seller/store/[sellerStoreId]/reservation/api/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from 'next/server'
 import {prisma} from "db";
 
-// Get store active reservations ['Pendiente', 'Reservado']
+// Get store active reservations ['Pendiente', 'Reservado', 'En camino']
 export async function GET(request: Request, { params }: { params: { sellerStoreId: string } }) {
     const storeId = parseInt(params.sellerStoreId)
 
     const storeReservations = await prisma.products_reservation.findMany({
         where: {
-            reservation_status: {code: {in: [1, 3]}},
+            reservation_status: {code: {in: [1, 3, 5]}},
             store_depots: {
                 store_id: storeId
             },
