99a5a5a26033b0c970162cfe1bd417e37fd53593
Alexis Manuel Hurtado García <alexismhgdotanime@gmail.com>
2023-12-09 18:44:10 +0100

    fixed === true

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import Login from "@/components/Login";
+import { Grid } from "@mui/material";
+
+export default function Home() {
+    return (
+        <main>
+            <Grid container>
+                <Login />
+            </Grid>
+        </main>
+    )
+}
---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,49 @@
+// @ts-nocheck
+
+import { prisma } from "db";
+import UserProfileMain from "@/app/profile/components/UserProfileMain";
+
+export default async function Page({ params }: { params: { id: number } }) {
+    const userId = parseInt(params.id)
+    const userDetails = await prisma.users.findUnique({ where: { id: userId }, include: { roles: true } })
+    const userRole = userDetails?.roles?.name ?? "user"
+
+    let ownerWarehouses = null
+    let ownerStores = null
+    let ownerProductsCount = null
+    let ownerWorkersCount = null
+
+    let sellerStores = null
+
+    if (userRole === "store_owner") {
+        const ownerWarehousesPromise = prisma.warehouses.findMany({ where: { owner_id: userId } })
+        const ownerStoresPromise = prisma.stores.findMany({ where: { owner_id: userId } })
+        const productsCountPromise = prisma.products.count({ where: { owner_id: userId } })
+        const workersCountPromise = prisma.users.count({ where: { work_for_user_id: userId } })
+
+        const ownerQueries = await Promise.all([ownerWarehousesPromise, ownerStoresPromise, productsCountPromise, workersCountPromise])
+        ownerWarehouses = ownerQueries[0]
+        ownerStores = ownerQueries[1]
+        ownerProductsCount = ownerQueries[2]
+        ownerWorkersCount = ownerQueries[3]
+        sellerStores = ownerQueries[1]
+    }
+
+    if (userRole === "store_seller") {
+        sellerStores = await prisma.stores.findMany({ where: { seller_user_id: userId } })
+    }
+
+    return (
+        <main>
+            <UserProfileMain
+                userDetails={userDetails}
+                userRole={userRole}
+                ownerWarehouses={ownerWarehouses}
+                ownerStores={ownerStores}
+                ownerProductsCount={ownerProductsCount}
+                ownerWorkersCount={ownerWorkersCount}
+                sellerStores={sellerStores}
+            />
+        </main>
+    )
+}
