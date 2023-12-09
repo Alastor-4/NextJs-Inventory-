019ba7794a5c9aa704cd0fd01bfaf6817290194e
dgabad <dgabad91@gmail.com>
2023-12-09 14:20:04 -0500

    register request declaration

--- src/app/api/auth/requests/auth.ts
+++ src/app/api/auth/requests/auth.ts
@@ -1,20 +1,21 @@
 import apiRequest from "@/api"
+import bcrypt from "bcrypt"
+import {notifyError} from "@/utils/generalFunctions";
 
 const url = "/api/auth/register"
 
 const auth = {
-    register: async function () {
+    register: async function ({username, password, name, mail, phone}: any) {
         try {
-            const response = await apiRequest.get(url)
+            const passwordHash = await bcrypt.hash(password, 10)
+            const response = await apiRequest.post(url, {username, passwordHash, name, mail, phone,})
             return response.data
-        } catch (e) {
-            //ToDo: notify error here
+        } catch(e) {
+            notifyError("Ha ocurrido un error en el proceso de registro del usuario")
         }
 
         return false
-    },
-
-    //ToDo: implement request for auth routes
+    }
 }
 
 export default auth;
