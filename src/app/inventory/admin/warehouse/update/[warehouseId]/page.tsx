571e9f4af9c11cd42cc5eab5f851c7f5b8df6244
alastor- <alexismhgdotanime@gmail.com>
2023-12-15 23:33:48 +0100

    fixed colons

--- src/app/inventory/admin/warehouse/update/[warehouseId]/page.tsx
+++ src/app/inventory/admin/warehouse/update/[warehouseId]/page.tsx
@@ -1,17 +1,17 @@
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/options";
 import WarehousesForm from "../../components/WarehousesForm";
+import { getServerSession } from "next-auth";
 import { prisma } from "db";
-import {getServerSession} from "next-auth";
-import {nextAuthOptions} from "@/app/api/auth/[...nextauth]/options";
 
 export default async function Page() {
-    const session = await getServerSession(nextAuthOptions)
-    const userId = session?.user.id
+    const session = await getServerSession(nextAuthOptions);
+    const userId = session?.user.id;
 
     const ownerUsers = await prisma.roles.findUnique({ where: { name: "store_owner" }, include: { users: true } })
 
     return (
         <main>
-            <WarehousesForm ownerUsers={ownerUsers?.users ?? []} userId={userId}/>
+            <WarehousesForm ownerUsers={ownerUsers?.users ?? []} userId={userId} />
         </main>
     )
 }
