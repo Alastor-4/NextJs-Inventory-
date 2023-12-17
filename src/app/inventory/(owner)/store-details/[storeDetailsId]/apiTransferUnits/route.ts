ae1484f0ce7e125e9509c99eef5db1ba0ad5fedd
dgabad <dgabad91@gmail.com>
2023-12-17 17:26:20 -0500

    fix issue

--- src/app/inventory/(owner)/store-details/[storeDetailsId]/apiTransferUnits/route.ts
+++ src/app/inventory/(owner)/store-details/[storeDetailsId]/apiTransferUnits/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/db";
 
-export async function GET(res: Response) {
-    const { searchParams } = new URL(res.url)
+export async function GET(req: Request) {
+    const { searchParams } = new URL(req.url)
     const productId = parseInt(<string>searchParams.get("productId"))
 
     const result = await prisma.warehouses.findMany({
@@ -24,8 +24,8 @@ export async function GET(res: Response) {
     return NextResponse.json(result);
 }
 
-export async function PUT(res: Response) {
-    const { depotId, product_total_remaining_units, storeDepotId, product_remaining_units, product_units } = await res.json()
+export async function PUT(req: Request) {
+    const { depotId, product_total_remaining_units, storeDepotId, product_remaining_units, product_units } = await req.json()
 
     const dataDepot = {
         product_total_remaining_units: product_total_remaining_units,
