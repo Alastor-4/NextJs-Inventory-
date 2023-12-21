3de5c9591eb9234aeee012cd361de6becfc862a8
hohemhein03 <hohemhein20@gmail.com>
2023-12-21 14:17:04 -0500

    fix seller_profit

--- src/app/inventory/(owner)/store/components/StoresForm.tsx
+++ src/app/inventory/(owner)/store/components/StoresForm.tsx
@@ -269,7 +269,7 @@ export default function StoresForm({ userId, storeId, sellerUsers }: { userId?: 
             online_catalog: activeCollection,
             online_reservation: activeReservations
         }
-
+        console.log(parseFloat(values.valueSellProfit))
         let response: any
 
         if (updateItem) {
