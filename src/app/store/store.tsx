af23edd0d9f0b8023bbd62f1cf067eca9ffd5f0e
alastor- <alexismhgdotanime@gmail.com>
2024-01-03 20:16:26 +0100

    new fields added to zustand store

--- src/app/store/store.tsx
+++ src/app/store/store.tsx
@@ -1,16 +1,21 @@
-import {create} from 'zustand'
+import { create } from 'zustand';
 
 type StoreType = {
-    loading: boolean,
-    startLoading: () => void,
-    stopLoading: () => void
+    loading: boolean;
+    startLoading: () => void;
+    stopLoading: () => void;
+    ownerProductsCount: number;
+    setProductsCount: (productsCount: number) => void;
 }
 
 export const useStore = create<StoreType>((set) => ({
     loading: false,
-    startLoading: () => set(() =>  ({ loading: true})),
-    stopLoading: () => set(() =>  ({ loading: false})),
+    ownerProductsCount: 0,
+    setProductsCount: (productsCount) => set(() => ({ ownerProductsCount: productsCount })),
+    startLoading: () => set(() => ({ loading: true })),
+    stopLoading: () => set(() => ({ loading: false })),
 }))
 
-export const startLoading = useStore.getState().startLoading
-export const stopLoading = useStore.getState().stopLoading
+export const startLoading = useStore.getState().startLoading;
+export const stopLoading = useStore.getState().stopLoading;
+export const setProductsCount = useStore.getState().setProductsCount;
