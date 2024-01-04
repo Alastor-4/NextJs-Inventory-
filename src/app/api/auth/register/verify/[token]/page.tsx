ebd16d766801cd74cbf8b7defef883f56d0dba15
dgabad <dgabad91@gmail.com>
2024-01-04 15:03:41 -0500

    fix verify token

--- src/app/api/auth/register/verify/[token]/page.tsx
+++ src/app/api/auth/register/verify/[token]/page.tsx
@@ -1,34 +1,34 @@
 'use client'
-import { Box, Button, Card, CardContent, Grid, Typography, styled } from '@mui/material';
+
+import { Button, Card, CardContent, Grid, Typography } from '@mui/material';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
 import axios from 'axios';
 
 const EmailVerification = () => {
-    const token = useParams();
-    const [verified, setVerified] = useState<boolean>(false);
+    const params = useParams();
+    const [verified, setVerified] = useState<boolean | null>(false);
     const [message, setMessage] = useState<{ message: string, color: string }>({ message: 'Validando...', color: "black" });
 
     useEffect(() => {
-        const validToken = async () => {
+        const verifyToken = async () => {
             try {
-                const isValidToken = await axios.get(`http://localhost:3000/api/auth/register?token=${token.token}`);
-                if ([201, 202].includes(isValidToken.status)) setMessage({ message: isValidToken.data, color: "green" });
+                const axiosResponse = await axios.get("/api/auth/register", {params: {token: params.token}})
+                if ([201, 202].includes(axiosResponse.status)) setMessage({ message: axiosResponse.data, color: "green" });
             } catch (error) {
                 setMessage({ message: "El token de verificación proporcionado no es correcto", color: "red" });
             }
             setVerified(true);
         }
-        !verified && validToken();
-    }, [token, verified]);
+        !verified && verifyToken();
+    }, [params, verified]);
 
     return (
         <Grid container justifyContent={"center"} flex={1} height={'100vh'} alignItems={"center"}>
             <Card>
                 <CardContent>
                     <Typography variant="h5" gutterBottom>
-                        Confirmando su correo electrónico
+                        Verificando datos
                     </Typography>
                     <Typography paragraph color={message.color}>
                         {message.message}
