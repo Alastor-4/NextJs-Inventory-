4a0e3a1dfaac3825eb6f0807ee52554fff7623b1
dgabad <dgabad91@gmail.com>
2024-01-06 11:51:20 -0500

    route change loader indicator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,41 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Roboto_Serif } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Roboto_Serif} from 'next/font/google'
 import ClientLayout from "@/app/clientLayout"
-import { Analytics } from '@vercel/analytics/react'
-import { AxiomWebVitals } from 'next-axiom'
+import {Analytics} from '@vercel/analytics/react'
+import {AxiomWebVitals} from 'next-axiom'
+import NextTopLoader from 'nextjs-toploader'
 // import SessionAuthProvider from '@/providers/SessionAuthProvider';
 // import SnackBarContextProvider from '@/providers/SnackbarProvider';
 
-const roboto = Roboto_Serif({ subsets: ['latin'] })
+const roboto = Roboto_Serif({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'Inventario Plus',
-  description: 'Inventario y Ventas Online',
-  manifest: "/manifest.json",
-  generator: "Next.js",
+    title: 'Inventario Plus',
+    description: 'Inventario y Ventas Online',
+    manifest: "/manifest.json",
+    generator: "Next.js",
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={`p-4 ${roboto.className}`}>
+    return (
+        <html lang="en">
+        <body className={`p-4 ${roboto.className}`}>
         {/* <SessionAuthProvider> */}
         {/* <SnackBarContextProvider> */}
+        <NextTopLoader color="#000" height={4}/>
         <ClientLayout>
-          {children}
+            {children}
         </ClientLayout>
-        <Analytics />
-        <AxiomWebVitals />
+        <Analytics/>
+        <AxiomWebVitals/>
         {/* </SnackBarContextProvider> */}
         {/* </SessionAuthProvider> */}
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
