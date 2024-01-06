d4d3a2aa52564ed268fe3dd6709ced5ef291a70e
dgabad <dgabad91@gmail.com>
2024-01-05 23:43:34 -0500

    pwa config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-const { withAxiom } = require('next-axiom');
+const withPWA = require('next-pwa')
+const { withAxiom } = require('next-axiom')
+const runtimeCaching = require('next-pwa/cache.js')
 
-module.exports = withAxiom({
-  experimental: {
-    serverActions: {allowedOrigins: ["*"]},
-  }
-})
+const isProduction = process.env.NODE_ENV === 'production';
+
+const pwaConfig = {
+  dest: 'public',
+  disable: false,
+  runtimeCaching
+}
+
+module.exports = withPWA({...pwaConfig})(withAxiom(
+    {
+      experimental: { serverActions: {allowedOrigins: ["*"]}},
+      ...pwaConfig,
+    }
+))
