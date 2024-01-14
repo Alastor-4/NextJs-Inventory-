5229d310a1b512a117b2a903c4055dbc646b9447
hohemhein03 <hohemhein20@gmail.com>
2024-01-14 02:20:30 -0500

    modified title to sell units

--- src/components/UpdateValueDialog.tsx
+++ src/components/UpdateValueDialog.tsx
@@ -1,12 +1,17 @@
 import { UpdateValueDialogProps } from "@/types/interfaces";
 import {
+    AppBar,
     Box,
     Button,
     Dialog,
     DialogActions,
     DialogContent,
     DialogTitle,
+    IconButton,
+    Toolbar,
+    Typography,
 } from "@mui/material";
+import { CloseIcon } from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
 import React from "react";
 
 export default function UpdateValueDialog({ dialogTitle, open, formik, setOpen, children }: UpdateValueDialogProps) {
@@ -18,15 +23,26 @@ export default function UpdateValueDialog({ dialogTitle, open, formik, setOpen, 
 
     return (
         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>{dialogTitle}</DialogTitle>
+            <AppBar sx={{ position: 'relative' }}>
+                <Toolbar>
+                    <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                        {dialogTitle}
+                    </Typography>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        onClick={handleClose}
+                        aria-label="close"
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
             <DialogContent>
                 <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap' }}>
                     {children}
                 </Box>
             </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>Cancelar</Button>
-            </DialogActions>
         </Dialog>
     )
 }
