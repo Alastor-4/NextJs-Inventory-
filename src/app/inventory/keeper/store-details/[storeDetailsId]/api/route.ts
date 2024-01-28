7ac5bfcab6af1d4cf39e61f9b9d2ac3b70a5fc64
alastor- <alexismhgdotanime@gmail.com>
2024-01-28 22:35:02 +0100

    added api and request for keeper store management

---
+++ src/app/inventory/keeper/store-details/[storeDetailsId]/api/route.ts
@@ -0,0 +1,108 @@
+import { NextResponse } from "next/server"
+import { prisma } from "@/db"
+
+//Sirve para abreviar params en las props de las peticiones
+interface Params {
+    params: { storeDetailsId: string }
+}
+
+//Pido todos los departamentos, productos de estos departamentos
+// y sus caracteristicas de la tienda seleccionada
+export async function GET(req: Request, { params }: Params) {
+    const storeId = parseInt(params.storeDetailsId)
+    const result = await prisma.departments.findMany({
+        //Elijo los departamentos de los productos de la tienda
+        where: {
+            products: {
+                some: {
+                    depots: {
+                        some: {
+                            store_depots: {
+                                some: {
+                                    store_id: storeId,
+                                    product_remaining_units: { not: -1 },
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        //Incluyo los productos q tiene la tienda respecto al departamento 
+        include: {
+            products: {
+                where: {
+                    depots: {
+                        some: {
+                            store_depots: {
+                                some: {
+                                    store_id: storeId,
+                                    product_remaining_units: { not: -1 },
+                                }
+                            }
+                        }
+                    }
+                },
+                // y en cada producto incluyo los detalles de ese
+                //producto
+                include: {
+                    depots: {
+                        include: {
+                            store_depots: {
+                                where: {
+                                    store_id: storeId,
+                                    product_remaining_units: { not: -1 },
+                                },
+                                include: { _count: { select: { product_offers: {where: {is_active: true}} } } }
+                            }
+                        }
+                    },
+                    departments: true,
+                    characteristics: true,
+                    images: true,
+                }
+            }
+        }
+
+    })
+    return NextResponse.json(result)
+}
+
+export async function PUT(req: Request) {
+    const {
+        id,
+        store_id,
+        depot_id,
+        product_units,
+        product_remaining_units,
+        seller_profit_percentage,
+        is_active,
+        sell_price,
+        sell_price_unit,
+        seller_profit_quantity,
+        price_discount_percentage,
+        price_discount_quantity,
+        discount_description
+    } = await req.json();
+
+    const result = await prisma.store_depots.update({
+        data: {
+            id,
+            store_id,
+            depot_id,
+            product_units,
+            product_remaining_units,
+            seller_profit_percentage,
+            is_active,
+            sell_price,
+            sell_price_unit,
+            seller_profit_quantity,
+            price_discount_percentage,
+            price_discount_quantity,
+            discount_description
+        }, where: { id: id }
+    })
+
+    return NextResponse.json(result)
+
+}
---
+++ src/app/inventory/keeper/store/update/api/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import { prisma } from "db";
+
+// GET store details by storeId
+export async function GET(req: Request) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const storeId = searchParams.get("storeId");
+
+        const store = await prisma.stores.findUnique({
+            where: { id: +storeId! },
+            include: {
+                seller_user: true,
+                store_open_days: true,
+                store_reservation_days: true
+            }
+        });
+
+        return NextResponse.json(store);
+    } catch (error) {
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
