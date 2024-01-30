a8c0ccda541a3b6c616a39f69ae14b16d96cd431
alastor- <alexismhgdotanime@gmail.com>
2024-01-30 21:18:12 +0100

    routes modifed, deleted unused parts

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,23 +1,17 @@
-import { prisma } from "@/db";
 import { NextResponse } from "next/server";
+import { prisma } from "@/db";
 
 export async function POST(req: Request) {
     try {
-        const body = await req.json();
-        const { username } = body;
+        const { username } = await req.json();
 
         if (!username) {
             return new NextResponse("Nombre es requerido", { status: 400 })
         }
-        const user = await prisma.users.findUnique({
-            where: { username }
-        })
-        if (!user) {
-            return new NextResponse("No existe usuario con ese nombre", { status: 400 })
-        }
+        const user = await prisma.users.findUnique({ where: { username } });
+
         return NextResponse.json(user);
     } catch (error) {
-        console.log('[USER_LOGIN]', error);
         return new NextResponse("Internal Error", { status: 500 })
     }
 }
--- src/app/api/auth/requests/auth.ts
+++ src/app/api/auth/requests/auth.ts
@@ -1,23 +1,31 @@
-import apiRequest from "@/api"
 import { notifyError, notifySuccess } from "@/utils/generalFunctions";
 import { hashPassword } from "@/utils/serverActions";
+import apiRequest from "@/api"
 
-const url = "/api/auth/register"
+const url = "/api/auth/register";
+const verifyRegisterDataUrl = "/api/auth/register/verify-register-data";
 
 const auth = {
-    register: async function ({ username, password1, name, mail, phone }: any) {
-
+    register: async function ({ username, password, name, email, phone }: any) {
         try {
-            const passwordHash = await hashPassword(password1);
-            const response = await apiRequest.post(url, { username, passwordHash, name, mail, phone, })
+            const passwordHash = await hashPassword(password);
+            const response = await apiRequest.post(url, { username, passwordHash, name, email, phone, });
             notifySuccess(response.data.message);
             return response.data
 
         } catch (e) {
-            notifyError("Ha ocurrido un error en el proceso de registro del usuario")
+            notifyError("Ha ocurrido un error en el proceso de registro del usuario");
         }
 
         return false;
+    },
+    verifyRegisterData: async function (username: string, phone: string, email: string) {
+        try {
+            const response = await apiRequest.post(verifyRegisterDataUrl, { username, phone, email });
+            return response.data;
+        } catch (e) {
+            notifyError("Ha ocurrido un error comprobando los datos de registro del usuario");
+        }
     }
 }
 
