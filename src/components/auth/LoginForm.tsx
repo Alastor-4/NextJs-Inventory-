73528c7871937c4ffef8c55d91ce6ff662b2e4eb
alastor- <alexismhgdotanime@gmail.com>
2024-01-30 21:17:25 +0100

    Login screen splitted

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,111 @@
+"use client"
+
+import { PersonOutlined, VisibilityOffOutlined, VisibilityOutlined } from "@mui/icons-material";
+import { Button, Grid, IconButton, InputAdornment, TextField } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+import { Formik } from "formik";
+import * as Yup from 'yup';
+
+const SignInForm = () => {
+    const router = useRouter();
+
+    const initialValues = { username: "", password: "" };
+
+    const signInValidationSchema = Yup.object({
+        username: Yup.string()
+            .required('Este campo es requerido')
+            .matches(/^\S*$/, 'No puede contener espacios')
+            .min(6, 'Debe contener al menos 6 caracteres')
+            .test('lowercase', 'Debe estar todo en minúsculas', (value) => {
+                return value === value.toLowerCase();
+            }),
+        password: Yup.string()
+            .required('Este campo es requerido')
+    })
+
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const toggleVisibility = () => setShowPassword(!showPassword);
+
+    return <Formik
+        initialValues={initialValues}
+        validationSchema={signInValidationSchema}
+        onSubmit={async ({ username, password }, { setFieldError }) => {
+            const responseNextAuth = await signIn('credentials', {
+                username,
+                password,
+                redirect: false,
+            });
+            if (responseNextAuth?.ok) router.push("/inventory");
+            if (responseNextAuth?.error === "Contraseña incorrecta") {
+                setFieldError("password", responseNextAuth.error);
+            } else { setFieldError("username", responseNextAuth?.error!) }
+        }}
+    >
+        {
+            ({ errors, touched, handleSubmit, getFieldProps }) => (
+                <Grid container item xs={12}>
+                    <form onSubmit={handleSubmit}>
+                        <Grid container item xs={12} spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Usuario"
+                                    size={"small"}
+                                    fullWidth
+                                    variant="outlined"
+                                    {...getFieldProps("username")}
+                                    error={!!errors.username && touched.username}
+                                    helperText={(errors.username && touched.username) && errors.username}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end" sx={{ mr: "6px" }}>
+                                                <PersonOutlined />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Contraseña"
+                                    size={"small"}
+                                    type={showPassword ? "text" : "password"}
+                                    fullWidth
+                                    variant="outlined"
+                                    {...getFieldProps("password")}
+                                    error={!!errors.password && touched.password}
+                                    helperText={(errors.password && touched.password) && errors.password}
+                                    InputProps={{
+                                        endAdornment:
+                                            <InputAdornment
+                                                position="end"
+                                                onClick={toggleVisibility}
+                                            >
+                                                <IconButton size={"small"}>
+                                                    {showPassword ? <VisibilityOffOutlined /> : <VisibilityOutlined />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button
+                                    color={"primary"}
+                                    type="submit"
+                                    variant={"contained"}
+                                    size={"large"}
+                                    sx={{ width: "100%" }}
+                                >
+                                    Entrar
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Grid>
+            )
+        }
+    </Formik >
+}
+
+export default SignInForm
---
+++ src/components/auth/RegisterForm.tsx
@@ -0,0 +1,190 @@
+'use client'
+
+import { Grid, Button, InputAdornment, IconButton, TextField } from '@mui/material';
+import { VisibilityOffOutlined, VisibilityOutlined } from '@mui/icons-material';
+import { handleKeyDown } from '@/utils/handleKeyDown';
+import auth from "@/app/api/auth/requests/auth";
+import { useState } from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const RegisterForm = ({ setShowRegisterForm }: { setShowRegisterForm: (bool: boolean) => void }) => {
+
+    const initialValues = {
+        username: '',
+        email: '',
+        name: '',
+        phone: '',
+        password: '',
+        confirmPassword: ''
+    }
+
+    const signUpValidationSchema = Yup.object({
+        username: Yup.string()
+            .required('Este campo es requerido')
+            .matches(/^\S*$/, 'No puede contener espacios')
+            .min(6, 'Debe contener al menos 6 caracteres')
+            .test('lowercase', 'Debe estar todo en minúsculas', (value) => {
+                return value === value.toLowerCase();
+            }),
+        email: Yup.string()
+            .required('Este campo es requerido')
+            .email('No es un correo válido'),
+        name: Yup.string()
+            .required('Este campo es requerido'),
+        phone: Yup.string()
+            .required('Este campo es requerido')
+            .matches(/^\d{8}$/, 'El teléfono debe tener 8 dígitos'),
+        password: Yup.string()
+            .required('Este campo es requerido')
+            .min(8, 'Debe contener al menos 8 caracteres'),
+        confirmPassword: Yup.string()
+            .required('Este campo es requerido')
+            .nullable()
+            .oneOf([Yup.ref("password"), null],
+                "Las contraseñas no coinciden")
+    })
+
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
+    const toggleVisibilityPassword = () => setShowPassword(!showPassword);
+    const toggleVisibilityConfirmPassword = () => setShowConfirmPassword(!showConfirmPassword);
+
+    return <Formik
+        initialValues={initialValues}
+        validationSchema={signUpValidationSchema}
+        onSubmit={async (data, { setFieldError }) => {
+            const messages: { "key": string, "message": string }[] = await auth.verifyRegisterData(data.username, data.phone, data.email);
+            if (messages.length === 0) {
+                auth.register(data);
+                setShowRegisterForm(false);
+            } else {
+                messages.forEach((({ key, message }) => setFieldError(key, message)));
+            }
+        }}
+    >
+        {
+            ({ handleSubmit, getFieldProps, touched, errors, isValid }) => (
+                <Grid item xs={12}>
+                    <form onSubmit={handleSubmit}>
+                        <Grid container item xs={12} spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Usuario*"
+                                    size={"small"}
+                                    fullWidth
+                                    autoComplete='off'
+                                    variant="outlined"
+                                    {...getFieldProps("username")}
+                                    error={!!errors.username && touched.username}
+                                    helperText={(errors.username && touched.username) && errors.username}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Nombre*"
+                                    size={"small"}
+                                    fullWidth
+                                    autoComplete='off'
+                                    variant="outlined"
+                                    {...getFieldProps("name")}
+                                    error={!!errors.name && touched.name}
+                                    helperText={(errors.name && touched.name) && errors.name}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Teléfono*"
+                                    size={"small"}
+                                    autoComplete='off'
+                                    onKeyDown={handleKeyDown}
+                                    inputMode="tel"
+                                    fullWidth
+                                    variant="outlined"
+                                    {...getFieldProps("phone")}
+                                    error={!!errors.phone && touched.phone}
+                                    helperText={(errors.phone && touched.phone) && errors.phone}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Correo*"
+                                    size={"small"}
+                                    fullWidth
+                                    autoComplete='off'
+                                    variant="outlined"
+                                    inputMode="email"
+                                    {...getFieldProps("email")}
+                                    error={!!errors.email && touched.email}
+                                    helperText={(errors.email && touched.email) && errors.email}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Contraseña*"
+                                    size={"small"}
+                                    type={showPassword ? "text" : "password"}
+                                    fullWidth
+                                    autoComplete='off'
+                                    variant="outlined"
+                                    {...getFieldProps("password")}
+                                    error={!!errors.password && touched.password}
+                                    helperText={(errors.password && touched.password) && errors.password}
+                                    InputProps={{
+                                        endAdornment:
+                                            <InputAdornment
+                                                position="end"
+                                                onClick={toggleVisibilityPassword}
+                                            >
+                                                <IconButton size={"small"}>
+                                                    {showConfirmPassword ? <VisibilityOutlined /> : <VisibilityOffOutlined />}
+                                                </IconButton>
+                                            </InputAdornment>,
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label="Confirmar Contraseña*"
+                                    size={"small"}
+                                    type={showConfirmPassword ? "text" : "password"}
+                                    fullWidth
+                                    autoComplete='off'
+                                    variant="outlined"
+                                    {...getFieldProps("confirmPassword")}
+                                    error={!!errors.confirmPassword && touched.confirmPassword}
+                                    helperText={(errors.confirmPassword && touched.confirmPassword) && errors.confirmPassword}
+                                    InputProps={{
+                                        endAdornment:
+                                            <InputAdornment
+                                                position="end"
+                                                onClick={toggleVisibilityConfirmPassword}
+                                            >
+                                                <IconButton size={"small"}>
+                                                    {showConfirmPassword ? <VisibilityOutlined /> : <VisibilityOffOutlined />}
+                                                </IconButton>
+                                            </InputAdornment>,
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button
+                                    color={"primary"}
+                                    type="submit"
+                                    variant={"contained"}
+                                    size={"large"}
+                                    sx={{ width: "100%" }}
+                                    disabled={!isValid}
+                                >
+                                    Registrar
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Grid>
+            )
+        }
+    </Formik>
+}
+
+export default RegisterForm
