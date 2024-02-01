62602a613f94b3f7d5064b316893cd97dad481e1
alastor- <alexismhgdotanime@gmail.com>
2024-02-01 05:16:19 +0100

    added two request function to auth request

--- src/app/api/auth/requests/auth.ts
+++ src/app/api/auth/requests/auth.ts
@@ -4,6 +4,7 @@ import apiRequest from "@/api"
 
 const url = "/api/auth/register";
 const verifyRegisterDataUrl = "/api/auth/register/verify-register-data";
+const urlChangePassword = "/api/auth/change-password";
 
 const auth = {
     register: async function ({ username, password, name, email, phone }: any) {
@@ -12,7 +13,6 @@ const auth = {
             const response = await apiRequest.post(url, { username, passwordHash, name, email, phone, });
             notifySuccess(response.data.message);
             return response.data
-
         } catch (e) {
             notifyError("Ha ocurrido un error en el proceso de registro del usuario");
         }
@@ -25,6 +25,29 @@ const auth = {
             return response.data;
         } catch (e) {
             notifyError("Ha ocurrido un error comprobando los datos de registro del usuario");
+        }
+    },
+    sendEmailToFindUser: async function (email: string) {
+        try {
+            const response = await apiRequest.post(urlChangePassword, { email });
+            if (response.status === 200) notifySuccess(response.data);
+            return response.data;
+        } catch (e) {
+            notifyError("Ha ocurrido un error comprobando si existe el usuario");
+        }
+    },
+    changePassword: async function (email: string, password: string) {
+        try {
+            const passwordHash = await hashPassword(password);
+
+            const response = await apiRequest.patch(urlChangePassword, { email, passwordHash });
+
+            if (response.status === 200) {
+                notifySuccess(response.data);
+            }
+            return response;
+        } catch (error) {
+            notifyError("Ha ocurrido un error en cambiando la contraseña");
         }
     }
 }
