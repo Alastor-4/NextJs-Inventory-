d3d3f2cc1fe5801d7971bd3ec600992a203d78a3
alastor- <alexismhgdotanime@gmail.com>
2024-02-01 19:38:29 +0100

    added some types to admin user page

--- src/app/inventory/owner/worker/api/route.ts
+++ src/app/inventory/owner/worker/api/route.ts
@@ -3,17 +3,13 @@ import { prisma } from "db";
 
 // Get all owner's user workers
 export async function GET(req: Request) {
-    const { searchParams } = new URL(req.url)
-    const ownerId = searchParams.get("ownerId")
+    const { searchParams } = new URL(req.url);
+    const ownerId = +searchParams.get("ownerId")!;
 
     if (ownerId) {
-        const users = await prisma.users.findMany(
-            {
-                where: { work_for_user_id: parseInt(ownerId) }, include: { roles: true }
-            }
-        )
+        const users = await prisma.users.findMany({ where: { work_for_user_id: ownerId }, include: { roles: true } });
 
-        return NextResponse.json(users)
+        return NextResponse.json(users);
     }
 
     return new Response('La acción de obtener los usuarios ha fallado', { status: 500 })
@@ -21,14 +17,14 @@ export async function GET(req: Request) {
 
 // Change owner's user role
 export async function PATCH(req: Request) {
-    const { searchParams } = new URL(req.url)
-    const userId = searchParams.get("userId")
-    const { roleId } = await req.json()
+    const { searchParams } = new URL(req.url);
+    const userId = +searchParams.get("userId")!;
+    const { roleId } = await req.json();
 
     if (userId) {
-        const updatedRole = await prisma.users.update({ data: { role_id: roleId }, where: { id: parseInt(userId) } })
+        const updatedRole = await prisma.users.update({ data: { role_id: roleId }, where: { id: userId } });
 
-        return NextResponse.json(updatedRole)
+        return NextResponse.json(updatedRole);
     }
 
     return new Response('La acción de modificar rol ha fallado', { status: 500 })
@@ -36,13 +32,13 @@ export async function PATCH(req: Request) {
 
 // Delete owner's user
 export async function DELETE(req: Request) {
-    const { searchParams } = new URL(req.url)
-    const userId = searchParams.get("userId")
+    const { searchParams } = new URL(req.url);
+    const userId = +searchParams.get("userId")!;
 
     if (userId) {
-        const updatedUser = await prisma.users.update({ data: { work_for_user_id: null, role_id: null }, where: { id: parseInt(userId) } })
+        const updatedUser = await prisma.users.update({ data: { work_for_user_id: null, role_id: null }, where: { id: userId } });
 
-        return NextResponse.json(updatedUser)
+        return NextResponse.json(updatedUser);
     }
 
     return new Response('La acción de eliminar trabajador ha fallado', { status: 500 })
