aa6c594eb8a2296e02796724196acc20b60a8637
dgabad <dgabad91@gmail.com>
2024-02-09 10:58:48 -0500

    properties api handlers

---
+++ src/app/inventory/seller/store/[sellerStoreId]/propertiesApi/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/db";
+
+interface Params {
+    params: { storeDepotId: string, propertyId: string }
+}
+
+// create a property for a store_depot
+export async function POST(req: Request, { params }: Params) {
+    const storeDepotId = parseInt(<string>params.storeDepotId)
+    const body = await req.json()
+    const {name, value} = body
+
+    const newProperty = await prisma.store_depot_properties.create({
+        data: {
+            store_depot_id: storeDepotId,
+            name: name,
+            value: value,
+        }
+    })
+
+    return NextResponse.json(newProperty)
+}
+
+// disable/enable a property for a store_depot
+export async function PUT(req: Request, { params }: Params) {
+    const propertyId = parseInt(<string>params.propertyId)
+
+    const body = await req.json()
+    const {value} = body
+
+    const newProperty = await prisma.store_depot_properties.update({
+        data: {is_active: value},
+        where: {
+            id: propertyId,
+        }
+    })
+
+    return NextResponse.json(newProperty)
+}
+
+// delete a property for a store_depot
+export async function DELETE(req: Request, { params }: Params) {
+    const propertyId = parseInt(<string>params.propertyId)
+
+    const deletedProperty = await prisma.store_depot_properties.delete({
+        where: {
+            id: propertyId,
+        }
+    })
+
+    return NextResponse.json(deletedProperty)
+}
