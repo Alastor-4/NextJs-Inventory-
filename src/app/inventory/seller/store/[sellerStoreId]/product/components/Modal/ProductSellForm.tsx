3bc380c661641f388f06ea251d799b6649ddfbde
alastor- <alexismhgdotanime@gmail.com>
2024-02-11 18:46:44 +0100

    ProductSell modal finished

--- src/app/inventory/seller/store/[sellerStoreId]/product/components/Modal/ProductSellForm.tsx
+++ src/app/inventory/seller/store/[sellerStoreId]/product/components/Modal/ProductSellForm.tsx
@@ -1,17 +1,18 @@
+//@ts-nocheck
 "use client"
 
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, FormHelperText, Grid, IconButton, MenuItem, TextField } from '@mui/material';
 import { CloseIcon } from 'next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon';
-import { computeDepotPricePerUnit, numberFormat } from '@/utils/generalFunctions';
+import { computeDepotPricePerUnit, notifySuccess, numberFormat } from '@/utils/generalFunctions';
 import { ProductSellFormProps, productsProps } from '@/types/interfaces';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import sellerStoreProduct from '../../requests/sellerStoreProduct';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import React, { useEffect, useMemo, useState } from 'react';
 import { esES } from '@mui/x-date-pickers/locales'
-import { Formik, useField } from 'formik';
+import { Formik } from 'formik';
 import * as Yup from "yup";
-import dayjs from "dayjs";
 import 'dayjs/locale/es';
 
 interface Product {
@@ -26,14 +27,15 @@ interface initialValuesProps {
     }[];
     paymentMethod?: string;
     description?: string;
-    payBefore?: dayjs.Dayjs;
+    payBefore?: Date;
 }
 
-const ProductSellForm = ({ isModalOpen, setIsOpen, isReceivable, selectedProducts, setSelectedProducts }: ProductSellFormProps) => {
+const ProductSellForm = ({ isModalOpen, storeId, setIsOpen, isReceivable, selectedProducts, allProductsByDepartment, setAllProductsByDepartment, setSelectedProducts, }: ProductSellFormProps) => {
 
     const handleClose = () => {
         setIsOpen(false);
     };
+
 
     const createFormValues = (products: productsProps[]) => {
         const productsArray = products.map((product) =>
@@ -44,12 +46,13 @@ const ProductSellForm = ({ isModalOpen, setIsOpen, isReceivable, selectedProduct
             products: productsArray,
             paymentMethod: "",
             description: "",
-            payBefore: dayjs(),
+            payBefore: undefined,
         };
     }
     const [initialValues, setInitialValues] = useState<initialValuesProps>(createFormValues(selectedProducts));
 
-    const paymentMethods = useMemo(() => ["Efectivo CUP", "Transferencia CUP", "Otro"], []);
+    const paymentMethods = useMemo(() => ["Efectivo CUP", "Transferencia CUP"], []);
+    // const paymentMethods = useMemo(() => ["Efectivo CUP", "Transferencia CUP", "Otro"], []);
 
     useEffect(() => {
         const handleOpenModal = () => {
@@ -66,7 +69,7 @@ const ProductSellForm = ({ isModalOpen, setIsOpen, isReceivable, selectedProduct
                 setInitialValues({
                     products: [...productsData],
                     description: "",
-                    payBefore: dayjs(),
+                    payBefore: new Date(),
                     paymentMethod: paymentMethods[0],
                 })
             } else {
@@ -114,8 +117,50 @@ const ProductSellForm = ({ isModalOpen, setIsOpen, isReceivable, selectedProduct
         payBefore: Yup.date().required("Este dato es requerido")
     });
 
-    const handleSell = async () => {
-        console.log("vendido");
+    const handleSell = async (values: initialValuesProps) => {
+        let sellProduct: { storeDepotId: number; unitsQuantity: number, price: number }[] = [];
+
+        values.products.forEach(({ unitsQuantity }: Product, index: number) => {
+            const storeDepot = selectedProducts[index].depots![0].store_depots![0];
+            sellProduct.push({
+                storeDepotId: storeDepot.id,
+                unitsQuantity: unitsQuantity,
+                price: computeDepotPricePerUnit(storeDepot, unitsQuantity) * unitsQuantity
+            });
+        });
+
+        const totalPrice = sellProduct.reduce((accumulate, current) => accumulate + current.price, 0);
+        const sell = { paymentMethod: values.paymentMethod!, totalPrice: totalPrice };
+        const data = { sellerStoreId: storeId, sellData: sell, sellProductsData: sellProduct };
+
+        let sellItemResponse;
+        if (isReceivable) {
+            const sellReceivableData = { description: values.description!, payBefore: values.payBefore! }
+            const newData = { ...data!, sellReceivableData }
+            sellItemResponse = await sellerStoreProduct.createSellReceivable(newData);
+        } else {
+            sellItemResponse = await sellerStoreProduct.sellStoreDepotManual(data);
+        }
+
+        if (sellItemResponse) {
+            const newDepartments = [...allProductsByDepartment!];
+
+            sellProduct.forEach(sellProductItem => {
+                for (const productsByDepartments of allProductsByDepartment!) {
+                    const departmentIndex = allProductsByDepartment!.indexOf(productsByDepartments);
+
+                    const productIndex = productsByDepartments.products!.findIndex((product: productsProps) => product.depots![0].store_depots![0].id === sellProductItem.storeDepotId);
+                    if (productIndex > -1) {
+                        newDepartments[departmentIndex].products![productIndex].depots![0].store_depots![0].product_remaining_units
+                            = allProductsByDepartment![departmentIndex].products![productIndex].depots![0].store_depots![0].product_remaining_units! - sellProductItem.unitsQuantity;
+                    }
+                }
+            })
+            setAllProductsByDepartment(newDepartments);
+            notifySuccess(isReceivable ? "Venta por cobrar registrada" : "La venta ha sido registrada");
+        }
+        setSelectedProducts([]);
+        handleClose()
     }
 
     const getTotals = (products: Product[]) => {
@@ -130,62 +175,6 @@ const ProductSellForm = ({ isModalOpen, setIsOpen, isReceivable, selectedProduct
         })
         return { totalProducts, totalPrice }
     }
-
-    // async function productsSell() {
-    //     let sellProduct: any[] = []
-
-    //     formik.values.productSell.products.forEach((item: any, index: number) => {
-    //         const storeDepot = selectedProducts[index].depots![0].store_depots![0];
-
-    //         sellProduct.push({
-    //             storeDepotId: storeDepot.id,
-    //             unitsQuantity: item.unitsQuantity,
-    //             price: computeDepotPricePerUnit(storeDepot, item.unitsQuantity) * item.unitsQuantity
-    //         });
-    //     })
-
-    //     const totalPrice = sellProduct.reduce((accumulate, current) => accumulate + current.price, 0);
-
-    //     const sell = { paymentMethod: formik.values.productSell.paymentMethod, totalPrice: totalPrice }
-
-    //     const data = {
-    //         sellerStoreId: storeId,
-    //         sellData: sell,
-    //         sellProductsData: sellProduct,
-    //     }
-
-    //     let sellItemResponse;
-    //     if (isSaleReceivable) {
-    //         const sellReceivableData = { description: formik.values.productSell.description, payBefore: formik.values.productSell.payBefore }
-    //         const newData = { ...data, sellReceivableData }
-    //         sellItemResponse = await sellerStoreProduct.createSellReceivable(newData)
-    //     } else {
-    //         sellItemResponse = await sellerStoreProduct.sellStoreDepotManual(data)
-    //     }
-
-    //     if (sellItemResponse) {
-    //         const newDepartments = [...allProductsByDepartment!];
-
-    //         sellProduct.forEach(sellProductItem => {
-    //             for (const productsByDepartments of allProductsByDepartment!) {
-    //                 const departmentIndex = allProductsByDepartment!.indexOf(productsByDepartments);
-
-    //                 const productIndex = productsByDepartments.products!.findIndex((product: productsProps) => product.depots![0].store_depots![0].id === sellProductItem.storeDepotId);
-    //                 if (productIndex > -1) {
-    //                     newDepartments[departmentIndex].products![productIndex].depots![0].store_depots![0].product_remaining_units
-    //                         = allProductsByDepartment![departmentIndex].products![productIndex].depots![0].store_depots![0].product_remaining_units! - sellProductItem.unitsQuantity;
-    //                 }
-    //             }
-    //         })
-
-    //         setAllProductsByDepartment(newDepartments);
-
-    //         notifySuccess(isSaleReceivable ? "Venta por cobrar registrada" : "La venta ha sido registrada");
-    //     }
-    //     formik.resetForm();
-    //     closeForm();
-    //     setSelectedProducts([]);
-    // }
 
     return (
         <Formik
@@ -291,32 +280,53 @@ const ProductSellForm = ({ isModalOpen, setIsOpen, isReceivable, selectedProduct
                                 </Grid>
 
                                 {isReceivable &&
-                                    <Grid item xs={12}>
-                                        <TextField
-                                            label="Descripción"
-                                            size={"small"}
-                                            fullWidth
-                                            {...getFieldProps("description")}
-                                            error={!!errors.description && touched.description}
-                                            helperText={(!!errors.description && touched.description) && !!errors.description}
-                                        />
-                                    </Grid>
-                                }
-
-                                {isReceivable &&
-                                    <Grid item xs={12} mx={"40px"}>
+                                    <Grid item xs={12} mt={"5px"}>
                                         <LocalizationProvider
                                             dateAdapter={AdapterDayjs}
                                             localeText={esES.components.MuiLocalizationProvider.defaultProps.localeText}
                                             adapterLocale='es'>
                                             <DemoContainer components={['DatePicker']}>
                                                 <DatePicker
-                                                    slotProps={{ field: { clearable: true }, toolbar: { hidden: true } }}
-                                                    label="Pagar antes de:" disablePast onChange={(value) => {
-                                                        setFieldValue("payBefore", value)
+                                                    slotProps={{
+                                                        field: { clearable: true },
+                                                        toolbar: { hidden: true },
+                                                        textField: {
+                                                            size: "small",
+                                                            margin: "dense",
+                                                            error: !!errors.payBefore && touched.payBefore,
+                                                        },
+                                                    }}
+                                                    label="Pagar antes de:" disablePast
+                                                    onChange={(value) => {
+                                                        setFieldValue("payBefore", value);
                                                     }} />
                                             </DemoContainer>
                                         </LocalizationProvider>
+                                        {!!errors.payBefore && (
+                                            <Grid item xs={12} textAlign={"center"}>
+                                                <FormHelperText component={"span"} sx={{ color: "#d32f2f" }}>
+                                                    {errors.payBefore}
+                                                </FormHelperText>
+                                            </Grid>
+                                        )}
+                                    </Grid>
+                                }
+                                {isReceivable &&
+                                    <Grid item xs={12} marginTop={"10px"}>
+                                        <TextField
+                                            label="Descripción"
+                                            size={"small"}
+                                            fullWidth
+                                            {...getFieldProps("description")}
+                                            error={!!errors.description && touched.description}
+                                        />
+                                        {!!errors.description && touched.description && (
+                                            <Grid item xs={12} textAlign={"center"}>
+                                                <FormHelperText component={"span"} sx={{ color: "#d32f2f" }}>
+                                                    {errors.description}
+                                                </FormHelperText>
+                                            </Grid>
+                                        )}
                                     </Grid>
                                 }
                             </Grid>
@@ -326,14 +336,17 @@ const ProductSellForm = ({ isModalOpen, setIsOpen, isReceivable, selectedProduct
                                 <Button color="primary"
                                     disabled={
                                         isReceivable ?
-                                            !!errors.products && !!touched.products
-                                            || !!errors.paymentMethod && touched.paymentMethod
-                                            || !!errors.payBefore && touched.payBefore
-                                            || !!errors.description || touched.description
-                                            : !!errors.products && !!touched.products
-                                            || !!errors.paymentMethod && !touched.paymentMethod
+                                            touched.paymentMethod === undefined ?
+                                                !touched.paymentMethod :
+                                                !!errors.products ||
+                                                !!errors.paymentMethod ||
+                                                !!errors.payBefore ||
+                                                !!errors.description
+                                            : touched.paymentMethod === undefined ?
+                                                !touched.paymentMethod :
+                                                !!errors.products
                                     }
-                                    variant="outlined" onClick={handleSell}
+                                    variant="outlined" onClick={() => { handleSell(values); resetForm(); }}
                                 >Vender</Button>
                             </DialogActions>
                         </DialogContent>
