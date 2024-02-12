e049a096132507da17e446b410a9734c66a21566
dgabad <dgabad91@gmail.com>
2024-02-11 23:40:01 -0500

    remove filters

--- src/app/inventory/owner/warehouse/[warehouseId]/transfer-history/components/WarehouseTransferHistory.tsx
+++ src/app/inventory/owner/warehouse/[warehouseId]/transfer-history/components/WarehouseTransferHistory.tsx
@@ -5,22 +5,16 @@ import {
     TableBody, TableCell, TableContainer, TableHead, TableRow,
     Toolbar, Typography
 } from '@mui/material';
-import { DatePicker, DateValidationError, LocalizationProvider, PickerChangeHandlerContext } from '@mui/x-date-pickers';
 import {
     ArrowLeft,
     ForwardToInbox,
-    HelpOutline,
     Mail
 } from '@mui/icons-material';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import {warehouseTransferProps} from '@/types/interfaces';
 import { TableNoData } from '@/components/TableNoData';
-import InfoTooltip from '@/components/InfoTooltip';
 import React, { useEffect, useState } from 'react';
-import { esES } from '@mui/x-date-pickers/locales'
 import { useRouter } from "next/navigation";
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import 'dayjs/locale/es';
 import warehouseTransfers
     from "@/app/inventory/owner/warehouse/[warehouseId]/transfer-history/requests/warehouseTransfers";
@@ -30,23 +24,16 @@ import {transactionToStore} from "@/utils/generalFunctions";
 
 dayjs.locale("es");
 
-const getMonth = (month: number) => { if (month < 9) return `0${month + 1}`; return month + 1 }
-const getDate = (date: number) => { if (date < 10) return `0${date}`; return date }
-
 const WarehouseTransferHistory = ({warehouseId}: {warehouseId: number}) => {
     const router = useRouter();
 
     const [allTransfers, setAllTransfers] = useState<warehouseTransferProps[] | null>(null);
-    const [sellDates, setSellDates] = useState<string[] | null>(null);
 
-    const [selectedDate, setSelectedDate] = useState<string>();
-
-    const handleChange = (value: unknown, context: PickerChangeHandlerContext<DateValidationError>) => {
-        if (value === null) { setSelectedDate(undefined); return }
-        const date = value as Dayjs;
-        const selectedDate = `${date.get("year")}-${getMonth(date.get('month'))}-${getDate(date.get('date'))}`
-        setSelectedDate(selectedDate);
-    };
+    function getLast30Days() {
+        const endDay = dayjs()
+        const startDay = endDay.subtract(30, "day")
+        return [startDay, endDay]
+    }
 
     useEffect(() => {
         const loadData = async () => {
@@ -58,26 +45,8 @@ const WarehouseTransferHistory = ({warehouseId}: {warehouseId: number}) => {
         loadData();
     }, [warehouseId]);
 
-    const refreshData = async () => {
-        const storeAllSellsDetails = await warehouseTransfers.allWarehouseTransfers(warehouseId);
-        setAllTransfers(storeAllSellsDetails);
-    }
 
     const handleNavigateBack = () => router.back();
-
-    const [showDetails, setShowDetails] = useState<number>(-1);
-
-    const [isOpenTooltip, setIsOpenTooltip] = useState<boolean>(false);
-    const handleTooltipClose = () => setIsOpenTooltip(false);
-    const handleTooltipOpen = () => setIsOpenTooltip(true);
-
-    const hasSell = (date: Dayjs): boolean => {
-        const dateToCompare: string = `${date.get("year")}-${getMonth(date.get('month'))}-${getDate(date.get('date'))}`;
-        if (sellDates) {
-            return !sellDates?.includes(dateToCompare);
-        }
-        return true;
-    }
 
     //Image handlers
     const [openImageDialog, setOpenImagesDialog] = useState<boolean>(false);
@@ -207,37 +176,6 @@ const WarehouseTransferHistory = ({warehouseId}: {warehouseId: number}) => {
             <Card variant={"outlined"}>
                 <CustomToolbar />
                 <CardContent>
-                    <Card variant={"outlined"} sx={{ padding: "10px", marginBottom: "10px" }}>
-                        <Grid item container alignItems="center" justifyContent="center" sx={{ marginTop: "-10px" }}>
-                            <Grid container item xs={"auto"} alignItems={"center"} >
-                                <Typography variant="subtitle1" sx={{ fontWeight: "400" }}>Búsqueda avanzada</Typography>
-                            </Grid>
-                            <Grid container item xs={"auto"} alignItems={"center"}>
-                                <InfoTooltip
-                                    isOpenTooltip={isOpenTooltip}
-                                    handleTooltipClose={handleTooltipClose}
-                                    message={"Puede buscar por fecha de venta"}
-                                >
-                                    <IconButton onClick={handleTooltipOpen}>
-                                        <HelpOutline />
-                                    </IconButton>
-                                </InfoTooltip>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} mx={"40px"}>
-                            <LocalizationProvider
-                                dateAdapter={AdapterDayjs}
-                                localeText={esES.components.MuiLocalizationProvider.defaultProps.localeText}
-                                adapterLocale='es'>
-                                <DemoContainer components={['DatePicker']}>
-                                    <DatePicker
-                                        disabled={sellDates?.length! > 0 ? false : true}
-                                        slotProps={{ field: { clearable: true }, toolbar: { hidden: true } }}
-                                        label="Seleccione una fecha" disableFuture shouldDisableDate={hasSell} onChange={handleChange} />
-                                </DemoContainer>
-                            </LocalizationProvider>
-                        </Grid>
-                    </Card>
                     <Card variant={"outlined"} sx={{ paddingTop: "20px", mx: "-5px" }}>
                         {
                             allTransfers?.length! > 0 ?
