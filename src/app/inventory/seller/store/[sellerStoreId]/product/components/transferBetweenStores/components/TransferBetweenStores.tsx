7bc9ad2f0fc363f1f9886d11bcc9c804d1a3b29a
dgabad <dgabad91@gmail.com>
2024-02-18 10:56:58 -0500

    refactoring

--- src/app/inventory/seller/store/[sellerStoreId]/product/components/transferBetweenStores/components/TransferBetweenStores.tsx
+++ src/app/inventory/seller/store/[sellerStoreId]/product/components/transferBetweenStores/components/TransferBetweenStores.tsx
@@ -42,99 +42,101 @@ const TransferBetweenStores = ({ storeId, storeDepot, badItem, cancelChecked, lo
     )
 
     const handleSubmit = async (values: any) => {
-        const data = {
-            store_depot_id: storeDepot.id,
-            units_transferred_quantity: parseInt(values.units),
-            from_store_accepted: true,
-            to_store_id: values.selectedStore,
-            to_store_accepted: false,
-            transfer_notes: values.text,
-            transfer_cancelled: false
-        }
-
         const dataStore = {
             id: storeDepot.id,
             product_remaining_units: storeDepot.product_remaining_units! - values.units
         }
 
         let result = await transfer.updateStore(storeId, dataStore);
+
         if (result) {
+            const data = {
+                store_depot_id: storeDepot.id,
+                units_transferred_quantity: parseInt(values.units),
+                from_store_accepted: true,
+                to_store_id: values.selectedStore,
+                to_store_accepted: false,
+                transfer_notes: values.text,
+                transfer_cancelled: false
+            }
+
             result = await transfer.createTransfer(storeId, data);
+
             if (result) {
                 notifySuccess("Se realizó la transferencia");
+
                 if (+values.units === storeDepot.product_remaining_units) cancelChecked(badItem);
+
                 loadData();
             }
         }
     }
 
     return (
-        <>
-            <Formik
-                initialValues={setInitialValues}
-                validationSchema={setValidationSchema}
-                onSubmit={handleSubmit}
-            >
-                {
-                    (formik: any) => (
-                        <form onSubmit={formik.handleSubmit}>
-                            <Grid container spacing={1.2}>
-                                <Grid item xs={12}>
+        <Formik
+            initialValues={setInitialValues}
+            validationSchema={setValidationSchema}
+            onSubmit={handleSubmit}
+        >
+            {
+                (formik: any) => (
+                    <form onSubmit={formik.handleSubmit}>
+                        <Grid container spacing={1.2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    fullWidth
+                                    name={'selectedStore'}
+                                    label={'Tienda'}
+                                    {...formik.getFieldProps('selectedStore')}
+                                    select
+                                    size='small'
+                                    error={formik.errors.selectedStore && formik.touched.selectedStore}
+                                    helperText={(formik.errors.selectedStore && formik.touched.selectedStore) && formik.errors.selectedStore}
+                                >
+                                    {
+                                        storeList?.map((store: stores, index: number) => (
+                                            <MenuItem key={index} value={store.id}>{store.name}</MenuItem>
+                                        ))
+                                    }
+                                </TextField>
+                            </Grid>
+                            <Grid item container alignItems={'center'}>
+                                <Grid item xs={6}>
                                     <TextField
-                                        fullWidth
-                                        name={'selectedStore'}
-                                        label={'Tienda'}
-                                        {...formik.getFieldProps('selectedStore')}
-                                        select
+                                        name={'units'}
+                                        label={'Unidades'}
+                                        {...formik.getFieldProps("units")}
                                         size='small'
-                                        error={formik.errors.selectedStore && formik.touched.selectedStore}
-                                        helperText={(formik.errors.selectedStore && formik.touched.selectedStore) && formik.errors.selectedStore}
-                                    >
-                                        {
-                                            storeList?.map((store: stores, index: number) => (
-                                                <MenuItem key={index} value={store.id}>{store.name}</MenuItem>
-                                            ))
-                                        }
-                                    </TextField>
-                                </Grid>
-                                <Grid item container alignItems={'center'}>
-                                    <Grid item xs={6}>
-                                        <TextField
-                                            name={'units'}
-                                            label={'Unidades'}
-                                            {...formik.getFieldProps("units")}
-                                            size='small'
-                                            error={formik.errors.units && formik.touched.units}
-                                            helperText={(formik.errors.units && formik.touched.units) && formik.errors.units}
-                                        />
-                                    </Grid>
-                                    <Grid item marginX={'auto'}  >
-                                        <Button
-                                            type='submit'
-                                            variant='contained'
-                                            size='small'
-                                            disabled={(formik.errors?.units || formik.errors?.selectedStore) ? true : false}
-                                        >
-                                            Aceptar
-                                        </Button>
-                                    </Grid>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        fullWidth
-                                        name={"text"}
-                                        label={'Mensaje'}
-                                        multiline
-                                        rows={4}
-                                        {...formik.getFieldProps('text')}
+                                        error={formik.errors.units && formik.touched.units}
+                                        helperText={(formik.errors.units && formik.touched.units) && formik.errors.units}
                                     />
                                 </Grid>
+                                <Grid item marginX={'auto'}  >
+                                    <Button
+                                        type='submit'
+                                        variant='contained'
+                                        size='small'
+                                        disabled={!formik.isValid}
+                                    >
+                                        Aceptar
+                                    </Button>
+                                </Grid>
                             </Grid>
-                        </form>
-                    )
-                }
-            </Formik>
-        </>
+                            <Grid item xs={12}>
+                                <TextField
+                                    fullWidth
+                                    name={"text"}
+                                    label={'Notas'}
+                                    multiline
+                                    rows={4}
+                                    {...formik.getFieldProps('text')}
+                                />
+                            </Grid>
+                        </Grid>
+                    </form>
+                )
+            }
+        </Formik>
     )
 }
 
